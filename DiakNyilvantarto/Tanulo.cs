f3c1d289388e45217139f945f3dd97727aa613d9
Nagy Gábor <nagyg76x@gmail.com>
2026-08-29 09:06:41 +0200

    Tanulo osztály létrehozva

---
+++ DiakNyilvantarto/Tanulo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DiakNyilvantarto
+{
+    internal class Tanulo
+    {
+        public string Nev {  get; set; } = string.Empty;
+
+        public int Eletkor { get; set; }
+
+        public string Osztaly { get; set; } = string.Empty;
+
+        public double Atlag {  get; set; }
+
+        public string Megjegyzes {  get; set; } = string.Empty;
+    }
+}
